e238814ab3c068c362efc293460d7b66381ce9fb
xcalder <xcalder@foxmail.com>
2018-12-02 15:35:24 +0800

    fix show captcha

--- catalog/catalog/libraries/Validate_code.php
+++ catalog/catalog/libraries/Validate_code.php
@@ -14,7 +14,7 @@ class Validate_code {
  private $fontcolor;//指定字体颜色
  //构造方法初始化
  public function __construct() {
-  $this->font = FCPATH . 'resources/public/resources/default/fonts/wqy-microhei-lite.ttc';//注意字体路径要写对，否则显示不了图片
+  $this->font = FCPATH . '../resources/public/resources/default/fonts/wqy-microhei-lite.ttc';//注意字体路径要写对，否则显示不了图片
  }
  //生成随机码
  private function createCode() {
